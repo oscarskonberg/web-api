ae7fca483e5e8e20f8f424a40da6b63e913e5ae4
Oscar Skønberg <73654020+oscarskonberg@users.noreply.github.com>
2024-10-18 19:36:30 +0200

    Oppdatert hovedfilen med begge deres filer

--- api-riktig-versjon.py
+++ api-riktig-versjon.py
@@ -1,7 +1,68 @@
 from flask import Flask, jsonify, request
+from neo4j import GraphDatabase #importerer GraphDatabase fra neo4j
 
 app = Flask(__name__)
 
+# Neo4j tilkoblingsdetaljer
+uri = "neo4j+s://c1a528e1.databases.neo4j.io"
+username = "neo4j"
+password = "S6TD4YbelBzmDilMjShCUveiQihCHR_84YgrY4v7NVE"
+
+driver = GraphDatabase.driver(uri, auth=(username, password))
+
+# Funksjon for å opprette en kunde
+def create_customer_in_neo4j(first_name, last_name, age, address):
+    with driver.session() as session:
+        session.run(
+            "CREATE (c:Customer {firstName: $firstName, lastName: $lastName, age: $age, address: $address})",
+            firstName=first_name, lastName=last_name, age=age, address=address
+        )
+
+# Funksjon for å hente alle kunder
+def get_customers_from_neo4j():
+    with driver.session() as session:
+        result = session.run("MATCH (c:Customer) RETURN c.firstName AS firstName, c.lastName AS lastName, c.age AS age, c.address AS address")
+        customers = []
+        for record in result:
+            customers.append({
+                "firstName": record["firstName"],
+                "lastName": record["lastName"],
+                "age": record["age"],
+                "address": record["address"]
+            })
+        return customers
+    
+# Flask-rute for å opprette en kunde
+@app.route('/customers', methods=['POST'])
+def create_customer():
+    data = request.get_json()
+    create_customer_in_neo4j(data['FirstName'], data['LastName'], data['age'], data['address'])
+    return jsonify({'message': 'Customer created successfully'}), 201
+
+# Flask-rute for å hente alle kunder
+@app.route('/customers', methods=['GET'])
+def get_customers():
+    customers = get_customers_from_neo4j()
+    return jsonify(customers)
+
+# Hent en kunde basert på etternavn
+@app.route('/customers/lastname/<string:last_name>', methods=['GET'])
+def get_customer_by_last_name(last_name):
+    with driver.session() as session:
+        result = session.run(
+            "MATCH (c:Customer {lastName: $lastName}) RETURN c.firstName AS firstName, c.lastName AS lastName, c.age AS age, c.address AS address", 
+            lastName=last_name
+        )
+        record = result.single()
+        if record:
+            customer = {
+                "firstName": record["firstName"],
+                "lastName": record["lastName"],
+                "age": record["age"],
+                "address": record["address"]
+            }
+            return jsonify(customer)
+    return jsonify({'message': 'Customer not found'}), 404
 
 @app.route('/add_car', methods=['POST'])
 def add_car():
@@ -116,7 +177,6 @@ def delete_employee(employee_id):
     employees = [emp for emp in employees if emp['employeeId'] != employee_id]
     return jsonify ({'message': 'Employee delete'}), 204
 
-#(local test) !!!!!!!!!!
-#if __name__ == '__main__':
-    #app.run(debug=True)
 
+if __name__ == '__main__':
+    app.run(debug=True)
